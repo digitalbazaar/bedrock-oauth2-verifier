8cbed00e4287e5ba5b78cc2da5a0f5d5e682a1a2
Dave Longley <dlongley@digitalbazaar.com>
2022-09-27 12:50:20 -0400

    Clean up API implementation and add tests.

---
+++ test/mocha/20-checkAccessToken.js
@@ -0,0 +1,129 @@
+/*!
+ * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ */
+import * as helpers from './helpers.js';
+import {checkAccessToken} from '@bedrock/oauth2-verifier';
+import {mockData} from './mock.data.js';
+
+const {baseUrl} = mockData;
+
+describe('checkAccessToken', () => {
+  const audience = 'test:audience';
+  const issuerConfigUrl = `${baseUrl}${mockData.oauth2IssuerConfigRoute}`;
+
+  it('passes on a valid token', async () => {
+    const accessToken = await helpers.getOAuth2AccessToken({audience});
+    const req = createRequest({accessToken});
+    let err;
+    let result;
+    try {
+      result = await checkAccessToken({req, issuerConfigUrl, audience});
+    } catch(e) {
+      err = e;
+    }
+    assertNoError(err);
+    should.exist(result);
+    result.should.have.keys(['protectedHeader', 'payload']);
+  });
+  it('fails with an expired token', async () => {
+    const accessToken = await helpers.getOAuth2AccessToken({
+      audience,
+      // expired 10 minutes ago
+      exp: Math.floor(Date.now() / 1000 - 600)
+    });
+    const req = createRequest({accessToken});
+    let err;
+    let result;
+    try {
+      result = await checkAccessToken({req, issuerConfigUrl, audience});
+    } catch(e) {
+      err = e;
+    }
+    should.exist(err);
+    should.not.exist(result);
+    err.status.should.equal(403);
+    err.data.type.should.equal('NotAllowedError');
+    should.exist(err.data.cause);
+    should.exist(err.data.cause.details);
+    should.exist(err.data.cause.details.code);
+    err.data.cause.details.code.should.equal('ERR_JWT_EXPIRED');
+    should.exist(err.data.cause.details.claim);
+    err.data.cause.details.claim.should.equal('exp');
+  });
+  it('fails on a future "nbf" claim', async () => {
+    const accessToken = await helpers.getOAuth2AccessToken({
+      audience,
+      // 10 minutes from now
+      nbf: Math.floor(Date.now() / 1000 + 600)
+    });
+    const req = createRequest({accessToken});
+    let err;
+    let result;
+    try {
+      result = await checkAccessToken({req, issuerConfigUrl, audience});
+    } catch(e) {
+      err = e;
+    }
+    should.exist(err);
+    should.not.exist(result);
+    err.status.should.equal(403);
+    err.data.type.should.equal('NotAllowedError');
+    should.exist(err.data.cause);
+    should.exist(err.data.cause.details);
+    should.exist(err.data.cause.details.code);
+    err.data.cause.details.code.should.equal(
+      'ERR_JWT_CLAIM_VALIDATION_FAILED');
+    should.exist(err.data.cause.details.claim);
+    err.data.cause.details.claim.should.equal('nbf');
+  });
+  it('fails on a bad "typ" claim', async () => {
+    const accessToken = await helpers.getOAuth2AccessToken({
+      audience,
+      typ: 'unexpected'
+    });
+    const req = createRequest({accessToken});
+    let err;
+    let result;
+    try {
+      result = await checkAccessToken({req, issuerConfigUrl, audience});
+    } catch(e) {
+      err = e;
+    }
+    should.exist(err);
+    should.not.exist(result);
+    err.status.should.equal(403);
+    err.data.type.should.equal('NotAllowedError');
+    should.exist(err.data.cause);
+    should.exist(err.data.cause.details);
+    should.exist(err.data.cause.details.code);
+    err.data.cause.details.code.should.equal(
+      'ERR_JWT_CLAIM_VALIDATION_FAILED');
+    should.exist(err.data.cause.details.claim);
+    err.data.cause.details.claim.should.equal('typ');
+  });
+  it('fails on a bad "iss" claim', async () => {
+    const accessToken = await helpers.getOAuth2AccessToken({
+      audience,
+      iss: 'urn:example:unexpected'
+    });
+    const req = createRequest({accessToken});
+    let err;
+    let result;
+    try {
+      result = await checkAccessToken({req, issuerConfigUrl, audience});
+    } catch(e) {
+      err = e;
+    }
+    should.exist(err);
+    should.not.exist(result);
+    err.status.should.equal(403);
+    err.data.type.should.equal('NotAllowedError');
+    should.exist(err.data.cause);
+    should.exist(err.data.cause.details);
+    should.exist(err.data.cause.details.code);
+    err.data.cause.details.code.should.equal(
+      'ERR_JWT_CLAIM_VALIDATION_FAILED');
+    should.exist(err.data.cause.details.claim);
+    err.data.cause.details.claim.should.equal('iss');
+  });
+});
---
+++ test/mocha/helpers.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2019-2022 Digital Bazaar, Inc. All rights reserved.
+ */
+import {importJWK, SignJWT} from 'jose';
+
+import {mockData} from './mock.data.js';
+
+export async function getOAuth2AccessToken({
+  audience, exp, iss, nbf, typ = 'at+jwt'
+}) {
+  const scope = 'read:/';
+  const builder = new SignJWT({scope})
+    .setProtectedHeader({alg: 'EdDSA', typ})
+    .setIssuer(iss ?? mockData.oauth2Config.issuer)
+    .setAudience(audience);
+  if(exp !== undefined) {
+    builder.setExpirationTime(exp);
+  } else {
+    // default to 5 minute expiration time
+    builder.setExpirationTime('5m');
+  }
+  if(nbf !== undefined) {
+    builder.setNotBefore(nbf);
+  }
+  const key = await importJWK({...mockData.ed25519KeyPair, alg: 'EdDSA'});
+  return builder.sign(key);
+}
+
+export function createRequest({accessToken}) {
+  const req = {
+    get() {
+      return `Bearer ${accessToken}`;
+    }
+  };
+  return req;
+}
